06cd8041c5df098245a9174429565d5f997a2b82
AvirukBasak <avirukbasak@gmail.com>
2025-01-08 14:57:44 +0530

    added a states file to maintain global state

---
+++ include/states.h
@@ -0,0 +1,16 @@
+//
+// Created by aviruk on 1/8/25.
+//
+
+#ifndef STATES_H
+#define STATES_H
+
+class States final {
+public:
+    States() = delete;
+
+    static int simulationSpeedScaler;
+    static int pixelMovementSpeedScaler;
+};
+
+#endif //STATES_H
---
+++ src/states.cpp
@@ -0,0 +1,8 @@
+//
+// Created by aviruk on 1/8/25.
+//
+
+#include "states.h"
+
+int States::simulationSpeedScaler = 1;
+int States::pixelMovementSpeedScaler = 1;
