4fdb194a7d3daa5113b5d6e6f87d7d5f1984426e
AvirukBasak <avirukbasak@gmail.com>
2025-01-11 00:39:58 +0530

    added class ControlPanel to display data and take interactive input

---
+++ include/components/ControlPanel.h
@@ -0,0 +1,16 @@
+//
+// Created by aviruk on 1/11/25.
+//
+
+#ifndef COMPONENTS_CONTROLPANEL_H
+#define COMPONENTS_CONTROLPANEL_H
+
+
+
+class ControlPanel {
+
+};
+
+
+
+#endif //COMPONENTS_CONTROLPANEL_H
---
+++ src/components/ControlPanel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aviruk on 1/11/25.
+//
+
+#include "components/ControlPanel.h"
