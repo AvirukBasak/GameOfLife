b49e75a9f3c7402fc5f9eb08b435abe1071872d5
AvirukBasak <avirukbasak@gmail.com>
2025-01-11 03:38:57 +0530

    added imgui-sfml v2.6 and dear imgui v1.90.6 sources in thirdparty

--- include/constants.h
+++ include/constants.h
@@ -9,7 +9,7 @@ class Constants final {
 public:
     Constants() = delete;
 
-    static constexpr const char *ASSETS_PATH = "../../assets";
+    static constexpr const char *ASSETS_PATH = "../assets";
     static constexpr const char *WINDOW_TITLE = "Rat in Maze";
     static constexpr float RENDER_FPS = 60.0f;
     static constexpr int WINDOW_WIDTH = 1200;
