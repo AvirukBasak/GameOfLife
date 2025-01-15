092a8c8528011df68530bee7a7124e49000fbdc8
AvirukBasak <avirukbasak@gmail.com>
2025-01-16 00:20:51 +0530

    used globbing in cmake files

--- src/imgui/ControlPanel.cpp
+++ src/imgui/ControlPanel.cpp
@@ -2,6 +2,8 @@
 // Created by aviruk on 1/11/25.
 //
 
+#include <cmath>
+
 #include "imgui.h"
 
 #include "constants.h"
@@ -34,46 +36,61 @@ void ui() {
     spacing(spacing2);
 
     // Simulation Speed slider
-    ImGui::Text("Simulation Speed");
-    spacing(spacing1);
-    ImGui::SliderFloat("##SimulationSpeed", &States::simulationSpeedScaler, 0, MAX_SIM_SPEED, "%.2f");
-
-    spacing(spacing2);
-
-    // Population Size slider
-    ImGui::Text("Population Size");
-    spacing(spacing1);
-    ImGui::SliderInt("##PopulationSize", &States::populationSize, 2, MAX_POPU_SIZE);
-
-    spacing(spacing2);
-
-    // Mutation Probability slider
-    float mutationPercentage = (float) States::mutationCount / (float) States::maxMutationCount * 100.f;
-    ImGui::Text("Mutation Percent: %d Gene%s", States::mutationCount, States::mutationCount == 1 ? "" : "s");
-    spacing(spacing1);
-    ImGui::SliderFloat("##MutationPercent", &mutationPercentage, 0, 100, "%.2f %%");
-    States::mutationCount = (int) ((float) States::maxMutationCount * mutationPercentage / 100.f);
-
-    spacing(spacing2);
-
-    // Mutation Probability slider
-    ImGui::Text("Mutation Probability");
-    spacing(spacing1);
-    ImGui::SliderFloat("##MutationProbability", &States::mutationProbability, 0.0f, 1.0f, "%.2f");
-
-    spacing(spacing2);
-
-    // Next Generation button
-    ImGui::Text("Current Generation: %d", States::currentGeneration);
-    spacing(spacing1);
-    if (ImGui::Button("Next Generation")) {
-        ++States::currentGeneration;
+    {
+        ImGui::Text("Simulation Speed");
+        spacing(spacing1);
+        ImGui::SliderFloat("##SimulationSpeed", &States::simulationSpeedScaler, 0, MAX_SIM_SPEED, "%.2f");
     }
 
     spacing(spacing2);
 
-    ImGui::Text("Current Highest Fitness: %d", States::highestFitnessThisGen);
-    ImGui::Text("Overall Highest Fitness: %d", States::highestOverallFitness);
+    // Population Size slider
+    {
+        ImGui::Text("Population Size");
+        spacing(spacing1);
+        ImGui::SliderInt("##PopulationSize", &States::populationSize, 2, MAX_POPU_SIZE);
+    }
+
+    spacing(spacing2);
+
+    // Mutation Probability slider
+    {
+        float mutationPercentage = (float) States::mutationCount / (float) States::maxMutationCount * 100.f;
+        ImGui::Text("Mutation Percent: %d Gene%s", States::mutationCount, States::mutationCount == 1 ? "" : "s");
+
+        spacing(spacing1);
+
+        ImGui::SliderFloat("##MutationPercent", &mutationPercentage, 0, 100, "%.2f %%");
+        States::mutationCount = (int) ((float) States::maxMutationCount * mutationPercentage / 100.f);
+    }
+
+    spacing(spacing2);
+
+    // Mutation Probability slider
+    {
+        ImGui::Text("Mutation Probability");
+        spacing(spacing1);
+        ImGui::SliderFloat("##MutationProbability", &States::mutationProbability, 0.0f, 1.0f, "%.2f");
+    }
+
+    spacing(spacing2);
+
+    // Next Generation button
+    {
+        ImGui::Text("Current Generation: %d", States::currentGeneration);
+        spacing(spacing1);
+        if (ImGui::Button("Next Generation")) {
+            ++States::currentGeneration;
+        }
+    }
+
+    spacing(spacing2);
+
+    // Additional info
+    {
+        ImGui::Text("Current Highest Fitness: %d", States::highestFitnessThisGen);
+        ImGui::Text("Overall Highest Fitness: %d", States::highestOverallFitness);
+    }
 }
 
 void ControlPanel::callImGuiComponents() {
