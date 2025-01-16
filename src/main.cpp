2134f5f5ecddb5ad83c25a7941283aa48574a496
AvirukBasak <avirukbasak@gmail.com>
2025-01-16 23:31:31 +0530

    corrected order of imgui fn calls as far as possible

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <imgui_internal.h>
 #include <iostream>
 #include <GL/gl.h>
 
@@ -12,6 +13,22 @@
 #include "classes/Game.h"
 #include "imgui/ControlPanel.h"
 
+// Font loading function should be called once after font creation
+inline void LoadFontToImGuiWithOpenGL() {
+    unsigned char *outPixels;
+    int outWidth, outHeight;
+    ImGui::GetIO().Fonts->GetTexDataAsRGBA32(&outPixels, &outWidth, &outHeight);
+
+    GLuint fontTextureID;
+    glGenTextures(1, &fontTextureID);
+    glBindTexture(GL_TEXTURE_2D, fontTextureID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, outWidth, outHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, outPixels);
+
+    ImGui::GetIO().Fonts->TexID = (void *) (intptr_t) fontTextureID;
+}
+
 int main() {
     // Initialize constants and states
     Constants::init();
@@ -23,91 +40,67 @@ int main() {
         Constants::WINDOW_TITLE,
         sf::Style::Titlebar | sf::Style::Close
     );
-    window.setFramerateLimit(Constants::RENDER_FPS);
 
-    ImGui::SetCurrentContext(ImGui::CreateContext());
-    ImGui_ImplOpenGL3_Init();
-    ImGui_ImplOpenGL3_NewFrame();
+    window.setFramerateLimit(Constants::RENDER_FPS);
 
     if (!ImGui::SFML::Init(window)) {
         std::cerr << "Failed to initialize ImGui!" << std::endl;
+        return -1;
     }
 
+    ImGui_ImplOpenGL3_Init();
+
+    // Load font
     States::defaultImguiFont = ImGui::GetIO().Fonts->AddFontFromFileTTF(States::defaultFontPath.c_str(), 15);
     if (!States::defaultImguiFont) {
         throw std::runtime_error(std::string("imgui: failed to load '").append(States::defaultFontPath).append("'"));
     }
 
-    // This is how you load a font in ImGui w/ SFML & OpenGL3 :)
-    {
-        unsigned char *outPixels;
-        int outWidth, outHeight;
-        ImGui::GetIO().Fonts->GetTexDataAsRGBA32(&outPixels, &outWidth, &outHeight);
+    // Load font texture ONCE after font is added
+    LoadFontToImGuiWithOpenGL();
 
-        GLuint fontTextureID;
-        glGenTextures(1, &fontTextureID);
-        glBindTexture(GL_TEXTURE_2D, fontTextureID);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, outWidth, outHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, outPixels);
-
-        // You can store the texture ID and pass it to the ImGui renderer
-        ImGui::GetIO().Fonts->TexID = (void *) (intptr_t) fontTextureID;
-    }
-
-    // Create a game object
+    // Create game object
     Game game(window);
-
-    // Create a controls object for ImGui components
     ControlPanel controlPanel;
 
     // Setup rendering interval
-    sf::Clock renderClock;
-    const sf::Time renderInterval = sf::milliseconds(1 / Constants::RENDER_FPS * 1000);
-
-    // Keep seperate clock for ImGui
-    sf::Clock imguiDeltaClock;
+    sf::Clock sfmlRenderDeltaClock;
+    sf::Clock imguiRenderDeltaClock;
+    const sf::Time renderInterval = sf::milliseconds(1000 / Constants::RENDER_FPS);
 
     // Main loop
     while (window.isOpen()) {
+        ImGui_ImplOpenGL3_NewFrame();
+        // ImGui::NewFrame(); <-- Not called as ImGui::SFML::Update() calls this
+
         sf::Event event{};
         while (window.pollEvent(event)) {
-            // Handle events in Game components
             game.handleEvent(event);
-            // Handle events in ImGui
             ImGui::SFML::ProcessEvent(window, event);
-            // Handle window closed event after to allow necessary cleanup above
             if (event.type == sf::Event::Closed) {
                 window.close();
-                return 0;
+                // Break to allow proper cleanup after window closed
+                break;
             }
         }
 
-        // Call game update function to update states on eaach iter
         game.update();
 
-        // Render frame RENDER_FPS times
-        if (renderClock.getElapsedTime() >= renderInterval) {
-            // Clear and render but dont display yet
+        if (sfmlRenderDeltaClock.getElapsedTime() >= renderInterval) {
             window.clear(sf::Color::Black);
             game.render();
-            renderClock.restart();
+            sfmlRenderDeltaClock.restart();
         }
 
-        // Call ImGui update
-        ImGui::SFML::Update(window, imguiDeltaClock.restart());
-
-        // Call ImGui functions
+        ImGui::SFML::Update(window, imguiRenderDeltaClock.restart());
         controlPanel.callImGuiComponents();
-
-        // Render ImGui last to display it over the SFML game
         ImGui::SFML::Render(window);
 
-        // Display with game and ImGui content
         window.display();
+        ImGui::EndFrame();
     }
 
-    ImGui::SFML::Shutdown();
     ImGui_ImplOpenGL3_Shutdown();
+    ImGui::SFML::Shutdown();
     return 0;
 }
